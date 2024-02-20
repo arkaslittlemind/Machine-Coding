dfe4dec20854cc7493d10459b37a9a1a867a2de3
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-02-21 02:20:24 +0530

    setting up routes and controllers

---
+++ Arka-API/controllers/products.js
@@ -0,0 +1,9 @@
+const getAllProducts = async (req, res) => {
+    res.status(200).json({ msg: 'Receiving all products' })
+};
+
+const getAllProductsTesting = async (req, res) => {
+    res.status(200).json({ msg: 'Receiving all products for testing' })
+}
+
+module.exports = {getAllProducts, getAllProductsTesting};
