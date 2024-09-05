86f503b52d670f81c9683254fb91aea0b14f9482
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-09-05 15:49:01 +0530

    marquee

---
+++ Marquee/src/App.jsx
@@ -0,0 +1,14 @@
+import Marquee from "react-fast-marquee";
+import "./App.css";
+import MarqueeComponent from "./components/Marquee";
+
+
+function App() {
+  return (
+    <Marquee>
+        <MarqueeComponent />
+    </Marquee>
+  )
+}
+
+export default App;
---
+++ Marquee/src/components/Marquee.jsx
@@ -0,0 +1,14 @@
+import Marquee from "react-fast-marquee"
+
+const MarqueeComponent = () => {
+  return (
+    <Marquee>
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+    </Marquee>
+  )
+}
+
+
+export default MarqueeComponent
